d736e6803c7fd31cc8c8a17b6c49200b3780b417
Orion Wills <orion.wills@gmail.com>
2019-04-02 15:43:50 -0500

    misc files

---
+++ acquire.py
@@ -0,0 +1,23 @@
+import env
+import pandas as pd
+
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    '''initiates sql connection'''
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+def get_titanic_data():
+    '''gets titanic data from sql dbase'''
+    return pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
+
+def get_iris_data():
+    '''gets iris data from sql dbase'''
+    return pd.read_sql('SELECT s.species_name, m.* FROM species s JOIN measurements m ON m.species_id = s.species_id', get_connection('iris_db'))
+
+def get_telco_data():
+    '''gets all telco data from sql dbase'''
+    return pd.read_sql('\
+    SELECT c.*, ct.contract_type, ist.internet_service_type, pt.payment_type FROM customers c\
+	JOIN contract_types ct ON c.contract_type_id = ct.contract_type_id\
+    JOIN internet_service_types ist ON c.internet_service_type_id = ist.internet_service_type_id\
+    JOIN payment_types pt ON c.payment_type_id = pt.payment_type_id',\
+    get_connection('telco_churn'))
---
+++ prepare.py
@@ -0,0 +1,50 @@
+'''
+misc functions for working with the titanic and iris dbase
+'''
+import pandas as pd
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+
+def drop_i_columns(df):
+    '''drops iris columns'''
+    return df.drop(columns = (['measurement_id', 'species_id']))
+
+def rename_species(df):
+    '''renames species_name to species'''
+    return df.rename(index = str, columns={'species_name': 'species'})
+
+def encode_species(df):
+    '''encodes species'''
+    encoder = LabelEncoder()
+    encoder.fit(df.species)
+    return df.assign(species_encode = encoder.transform(df.species))
+
+def prep_iris(df):
+    '''preps all of iris data'''
+    return df.pipe(drop_i_columns)\
+        .pipe(rename_species)\
+        .pipe(encode_species)
+
+def handle_missing_values(df):
+    '''sets missing values in embarked and embark_town'''
+    return df.assign(
+        embark_town = df.embark_town.fillna('Other'),
+        embarked = df.embarked.fillna('O')
+    )
+
+def drop_t_columns(df):
+    '''drop deck'''
+    return df.drop(columns = 'deck')
+
+def encode_embarked(df):
+    '''encodes embarked'''
+    encoder=LabelEncoder()
+    encoder.fit(df.embarked)
+    return df.assign(embarked_encode = encoder.transform(df.embarked))
+
+def prep_titanic(df):
+    '''preps all of titanic'''
+    return df.pipe(handle_missing_values)\
+        .pipe(drop_t_columns)\
+        .pipe(encode_embarked)
