16c9abf93d7cd7538c80a6b3a15fe3a1fa64541d
Matthew Capper <matthew.capper01@gmail.com>
2019-04-04 08:08:15 -0500

    4_4 808am

---
+++ data_dictionary.py
@@ -0,0 +1,128 @@
+''' Data Dictionary:
+customer_id:
+    The identification number of any client in the telco dataset.
+gender:
+    The identified gender of any customer in the telco dataset.
+    listed in english as 'Male' or 'Female.'
+senior_citizen:
+    The status of a customer as a senior citizen in binary value.
+    0: Not a senior
+    1: senior citizen.
+partner:
+    Status of whether or not a customer has a domestic partner.
+    listed in Yes/No values.
+dependents:
+    Status of whether or not a customer has dependents.
+    listed in Yes/No values.
+tenure:
+    How long the customer has been with the company in months
+phone_service:
+    Status of whether or not the customer has phone service
+    Yes/No values
+multiple_lines:
+    Status of whether or not customer has multiple phone lines.
+    Lists 'no phone service' if phone_service value is 'No'
+internet_service_type:
+    Indication of what type of internet service customer has.
+    DSL, Fiber, or no internet.
+internet_service_type_id:
+    value by number of what type of internet service customer has.
+    1: DSL
+    2: Fiber
+    3: No service
+    ~~after remapping (int_type_id:)
+    0: No service
+    1:DSL
+    2: Fiber
+online_security:
+    Status of whether or not customer has online security service enabled.
+    Yes/No
+online_backup:
+    Status of whether or not customer has online backup services.
+    Yes/No
+device_protection:
+    Status of whether or not customer has device protection service.
+    Yes/No
+tech_support:
+    Status of whether or not customer has tech_support service.
+    Yes/No
+streaming_tv:
+    Status of whether or not customer has streaming television services
+    Yes/No
+streaming_movies:
+    Status of whether or not customer has streaming movie services.
+    Yes/No
+contract_type:
+    Type of contract customner is utilizing service on.
+    month-to-month, one-year contract, or two-year contract.
+contract_type_id:
+    Numeral represending what type of contract a customer is on
+    0: month-to-month
+    1: one-year contract
+    2: two-year contract
+paperless_billing:
+    Status of whether or not customer is enrolled in paperless billing.
+    Yes/No.
+payment_type:
+    What type of method a customer uses to pay for service.
+    Electronic Check, Mailed Check, Automatic Bank Transfer, 
+    or Automatic Credit Card Payment.
+payment_type_id:
+    Numeral value indicating what type of payment type customer utilizes.
+    1: Electronic Cheque
+    2: Mailed Cheque
+    3: Automatic Bank Transfer
+    4: Automatic Credit Card Payment
+monthly_charges:
+    Current amount in USD what customer is paying for services by month
+total_charges:
+    Total amount customer has payed thus far in accrued monthly bills
+    and/or charges
+churn:
+    Whether or not a customer has churned, or in lamens terms, 
+    left the company and discontinued service.
+    Represented as Yes/No inititally, altered to:
+    0: Not Churned
+    1: Churned
+phone_id:
+    Combination of phone service status and multiple phone line status.
+    0: No service
+    1: One line
+    2: 2 or more lines
+household_type_id:
+    Combination of partner and dependent status.
+    0: No partner or dependents
+    1: Partner with no dependents
+    2: Dependent but no partner
+    3: partner and dependent(s)
+streaming_services:
+    Combination of streaming movie and streaming tv services.
+    0: No streaming service
+    1: Streaming 
+    2: Streaming
+    3: Both streaming services
+online_security_backup:
+    Combination of online security and backup services
+    0:
+    1:
+    2:
+    3:
+gender_e:
+    enumerated version of gender column.
+    0: Female
+    1: Male
+
+device_protection_e:
+    Enumerated version of device protection service.
+    0: No device protection
+    1: Device protection
+
+tech_support_e:
+    Enumerated version of tech support services
+    0: No tech support service
+    1: tech support service
+
+paperless_billing_e:
+    Enumerated version of paperless billing category.
+    0: Not enrolled in paperless
+    1: enrolled in paperless
--- telco_prep.py
+++ telco_prep.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 # binning and value_counts: 
 def value_counts(dataframe):
+    ''' assesses numerical/continuous data in telco and bins if appropriate'''
     for col in dataframe.drop(columns=('customer_id')):
         if np.issubdtype(dataframe[col].dtype, np.number) and dataframe[col].nunique() > 10:
             print(dataframe[col].value_counts(bins=10, sort=False))
@@ -16,27 +17,45 @@ def value_counts(dataframe):
 
 
 def handle_missing_values(dataframe):
+    '''removes customers with total charges of 0, i.e. customers that haven't been with the company long
+    enough to glean information about churn from as they just signed up (very small portion of dataset)'''
     return dataframe.assign(
         total_charges = dataframe.total_charges.dropna(0)
     )
 
 def churn_num(dataframe):
+    '''enumerates churn column from a yes/no to 0 or 1 for better analysis.  maps explicit values.'''
     return dataframe.assign(
         churn = dataframe['churn'].map({'No': 0, 'Yes': 1})
 
     )
 
 def tenure_year(dataframe):
+    '''creates a new column in telco dataframe called tenure_year that assesses what year of service a 
+    customer is in at the time of snapshot'''
     dataframe['tenure_year'] = (dataframe.tenure / 12).astype(int) + 1
     return dataframe
 
+
+def sort_int_service(df):
+    '''re-orders our internet service type id into a new column for better correlative analysis
+    no internet service is remapped to 0 and incrementing services ++ from there'''
+    df['int_type_id'] = df['internet_service_type_id'].replace({3: 0, 2: 2, 1: 1})
+    return df
+
+# setting up choices for logical conditions in coming function: 
 choices_lns = [2, 1, 0]
 choices = [3, 2, 1, 0]
 
 
 
 
+
 def conditional_encodes(df):
+    '''creates four new columns for our dataset that enumerates values based on a combination of data
+    from two separate columns into each new column'''
+
+    # Combining online security service with online backup service:
     conditions_secback = [
     (df['online_security'] == 'Yes') & (df['online_backup'] == 'Yes'),
     (df['online_security'] == 'No') & (df['online_backup'] == 'Yes'), 
@@ -44,6 +63,7 @@ def conditional_encodes(df):
     (df['online_security'] == 'No') & (df['online_backup'] == 'No')
     ]
 
+    # Combining streaming tv service and streaming movie service:
     conditions_strm = [
     (df['streaming_tv'] == 'Yes') & (df['streaming_movies'] == 'Yes'),
     (df['streaming_tv'] == 'No') & (df['streaming_movies'] == 'Yes'), 
@@ -51,6 +71,7 @@ def conditional_encodes(df):
     (df['streaming_tv'] == 'No') & (df['streaming_movies'] == 'No')
     ]
 
+    # Combining partner and dependents status in household:
     conditions_pdep = [
     (df['partner'] == 'Yes') & (df['dependents'] == 'Yes'),
     (df['partner'] == 'No') & (df['dependents'] == 'Yes'), 
@@ -58,12 +79,13 @@ def conditional_encodes(df):
     (df['partner'] == 'No') & (df['dependents'] == 'No')
     ]
 
+    # Combining phone service status with multiple phone line status:
     conditions_lns = [
     (df['phone_service'] == 'Yes') & (df['multiple_lines'] == 'Yes'),
     (df['phone_service'] == 'Yes') & (df['multiple_lines'] == 'No'),
     (df['phone_service'] == 'No') & (df['multiple_lines'] == 'No')
     ]
-
+    # Implementing logical statements into new columns utilizing numpy select:
     df['phone_id'] = np.select(conditions_lns, choices_lns)
     df['household_type_id'] = np.select(conditions_pdep, choices)
     df['streaming_services'] = np.select(conditions_strm, choices)
@@ -71,37 +93,44 @@ def conditional_encodes(df):
     return df
 
 def encode_gender(df):
+    '''encodes gender column into a new enumerated column'''
     encoder=LabelEncoder()
     encoder.fit(df.gender)
     return df.assign(gender_e = encoder.transform(df.gender))
 
 def encode_tech(df):
+    '''encodes tech support column into a new enumerated column'''
     encoder=LabelEncoder()
     encoder.fit(df.tech_support)
     return df.assign(tech_support_e = encoder.transform(df.tech_support))
 
 def encode_paperless(df):
+    '''encodes paperless billing column into new enumerated column'''
     encoder=LabelEncoder()
     encoder.fit(df.paperless_billing)
     return df.assign(paperless_billing_e = encoder.transform(df.paperless_billing))
 
 def encode_device_protection(df):
+    '''encodes device protection column into new enumerated column'''
     encoder=LabelEncoder()
     encoder.fit(df.device_protection)
     return df.assign(device_protection_e = encoder.transform(df.device_protection))
 
 def format_totals(df):
+    '''formats total charges from a string object to a numeric type'''
     df['total_charges'] = df['total_charges'].convert_objects(convert_numeric=True)
     df.total_charges.dropna(0, inplace=True)
     return df
 
 def drop_cols(df):
+    '''drops customer_id and non-numeric versions of existing columns for exploratory analysis purposes'''
     return df.drop(columns=(['customer_id', 'partner', 'dependents', 'phone_service',
     'multiple_lines', 'online_security', 'online_backup',
     'streaming_tv', 'gender','streaming_movies', 'contract_type',
-    'internet_service_type', 'payment_type', 'tech_support', 'paperless_billing', 'device_protection']))
+    'internet_service_type', 'internet_service_type_id', 'payment_type', 'tech_support', 'paperless_billing', 'device_protection']))
 
 def prep_telco_data(df):
+    '''pipes all but drop_cols and value_counts to groom our initial dataset'''
     return df.pipe(handle_missing_values)\
     .pipe(churn_num)\
     .pipe(tenure_year)\
@@ -110,5 +139,6 @@ def prep_telco_data(df):
     .pipe(encode_device_protection)\
     .pipe(encode_tech)\
     .pipe(encode_paperless)\
+    .pipe(sort_int_service)\
     .pipe(format_totals)
 
